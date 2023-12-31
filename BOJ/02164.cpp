9983b7a3e25e552b3f4d6914ddbc2c61cb1bd3b2
doorcs <domybestat@pusan.ac.kr>
2023-12-31 15:30:35 +0900

    2164번: 카드2

--- BOJ/02164.cpp
+++ BOJ/02164.cpp
@@ -10,7 +10,7 @@ int main() {
     for(int i=1; i<=a; i++) q.push(i);
 
     for(int i=0; i<a-1; i++){
-    q.pop();
+    q.pop(); // queue의 pop은 리턴값이 없다. queue는 FIFO이므로 front가 삭제됨
     q.push(q.front());
     q.pop();
     }
