14beb0677b733676d61ef01e6f8c5cda68817176
doorcs <domybestat@pusan.ac.kr>
2024-01-09 01:49:29 +0900

    실습 056: this 포인터

---
+++ C++프로그래밍과실습/056_thisPointer_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "056_thisPointer.h"
+
+int main() {
+  std::string myString;
+  std::getline(std::cin, myString);
+  auto tokens =
+      StringSplitter(myString)
+      .trim()
+      .removeNonAlnum()
+      .split(' ');
+
+  for (auto& token : tokens)
+    std::cout << token << std::endl;
+
+  return 0;
+}
