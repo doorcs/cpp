459e1cc9622f11ac248b9f659d669a4cb425164c
doorcs <domybestat@pusan.ac.kr>
2024-01-10 00:02:16 +0900

    실습 055: static member variable

---
+++ C++프로그래밍과실습/055_staticMemberVariable.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum LogLevel {
+    DEBUG = 0,
+    INFO = 1,
+    WARNING = 2,
+    ERROR = 3
+};
+
+class Logger {
+    // implement your code
+public:
+    Logger(int level){
+        logLevel=level;
+    }
+    ~Logger(){logLevel=1;}
+    static void setLogLevel(int level);
+    static void debug(const string&, const string&);
+    static void info(const string&, const string&);
+    static void warning(const string&, const string&);
+    static void error(const string&, const string&);
+private:
+    static int logLevel;
+};
