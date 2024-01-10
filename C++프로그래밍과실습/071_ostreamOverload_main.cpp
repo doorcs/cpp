db5b39c7a7e3d1327788bb12b9092cb6aafcbc85
doorcs <domybestat@pusan.ac.kr>
2024-01-11 00:56:17 +0900

    실습 071: 출력 연산자 (연산자 << 오버로딩)

---
+++ C++프로그래밍과실습/071_ostreamOverload_main.cpp
@@ -0,0 +1,9 @@
+#include "071_ostreamOverload.h"
+
+int main() {
+    Complex c1(3, 2), c2(1, 1);
+    Complex c3(c1);
+    std::cout << c1 << std::endl;
+    std::cout << c2 << std::endl;
+    std::cout << c3 << std::endl;
+}
