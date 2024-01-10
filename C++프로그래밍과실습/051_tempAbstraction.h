8294a81470d4ceb77ebabf4c05a0d6499cb56421
doorcs <domybestat@pusan.ac.kr>
2024-01-11 00:02:42 +0900

    실습 051: Abstraction - Temperature

---
+++ C++프로그래밍과실습/051_tempAbstraction.h
@@ -0,0 +1,18 @@
+enum SCALE {
+    CELSIUS,
+    FAHRENHEIT
+};
+
+class Temperature {
+public:
+    Temperature(double temp, SCALE scale=CELSIUS);
+    Temperature add(Temperature other);
+    std::string print() const;
+private:
+    // Conversion Function
+    void convertToCelsius();
+    void convertToFahrenheit();
+private:
+    double temperature;
+    SCALE scale;
+};
