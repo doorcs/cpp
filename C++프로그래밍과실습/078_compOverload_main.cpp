8153fb1212fb888bb0f13b0d3ef02a73b03e82c2
doorcs <domybestat@pusan.ac.kr>
2024-01-12 23:59:25 +0900

    실습 078: < 연산자 오버로딩
    std::map<key, value>에 key로 접근할 때 주의할 점!!

--- C++프로그래밍과실습/078_compOverload_main.cpp
+++ C++프로그래밍과실습/078_compOverload_main.cpp
@@ -4,9 +4,10 @@ int main() {
     std::map<int, Student> student_grades;
 
     // Insert 5 student objects into the map
-    student_grades[1] = {1, "Bob", 90}; // key를 이용해 맵에 접근할 때, 맵에 해당 key가 존재하지 않는다면 맵은 value의 기본 생성자를 호출하기 때문에
-    // map[1] = Student(); 
-    // map[1] = {1, "Bob", 90}; 사실은  두 단계로 나누어 실행되는 셈이다. 따라서 클래스의 기본 생성자`Student()`가 정의되어 있지 않다면 에러가 발생한다.
+
+    student_grades[1] = {1, "Bob", 90}; // key를 이용해 맵에 접근할 때, 맵에 해당 key가 존재하지 않는다면 맵은 자동으로 value의 기본 생성자를 호출하기 때문에
+//  student_grades[1] = Student(); 
+//  student_grades[1] = {1, "Bob", 90}; 사실은  두 단계로 나누어 실행되는 셈이다. 따라서 클래스의 기본 생성자`Student()`가 정의되어 있지 않다면 에러가 발생한다.
 
     // student_grades.emplace(1, Student{1, "Bob", 90}); 처럼 사용하면 기본 생성자가 없어도 오류가 발생하지 않는다!
     student_grades[2] = {2, "Alice", 85};
