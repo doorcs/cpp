5c3b16b168adee416a448c0b9c24f968fcc4811e
성문 <castledoor@pusan.ac.kr>
2024-01-18 01:49:56 +0900

    실습 092: override (virtual function)

---
+++ C++프로그래밍과실습/092_overrideVirtualFunction.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class Vehicle{
+public:
+    virtual void accelerate() = 0;
+};
+
+class GV70: public Vehicle{
+    string name;
+    int speed;
+public:
+    GV70(): name("gv70"), speed(30){ }
+
+    void accelerate(){
+        cout << "The speed of the " << name << " has increased by " << speed << " km/h." << endl;
+    }
+};
+
+class Grandeur: public Vehicle{
+    string name;
+    int speed;
+public:
+    Grandeur(): name("grandeur"), speed(20){ }
+
+    void accelerate(){
+        cout << "The speed of the " << name << " has increased by " << speed << " km/h." << endl;
+    }
+};
+
+class Carnibal: public Vehicle{
+    string name;
+    int speed;
+public:
+    Carnibal(): name("carnibal"), speed(10){ }
+
+    void accelerate(){
+        cout << "The speed of the " << name << " has increased by " << speed << " km/h." << endl;
+    }
+};
+// 아래는 제공된 코드입니다.
+unique_ptr<Vehicle> make_vehicle() {
+    std::string choice;
+    cin >> choice;
+
+    if (choice == "gv70") return make_unique<GV70>();
+    else if (choice == "grandeur") return make_unique<Grandeur>();
+    else if (choice == "carnibal") return make_unique<Carnibal>();
+    else return make_unique<GV70>();
+}
+
+int main() {
+    vector<unique_ptr<Vehicle>> test_vehicles;
+    test_vehicles.push_back(make_vehicle()); //gv70
+    test_vehicles.push_back(make_vehicle()); //grandeur
+    test_vehicles.push_back(make_vehicle()); //carnibal
+
+    for (auto& v : test_vehicles)
+            (*v).accelerate();
+}
