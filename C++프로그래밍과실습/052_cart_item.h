453c82c40752b543240a168eb896ef470013f03e
성문 <castledoor@pusan.ac.kr>
2024-01-20 02:40:32 +0900

    과제 052: 객체를 이용한 Cart

---
+++ C++프로그래밍과실습/052_cart_item.h
@@ -0,0 +1,19 @@
+class Item {
+public:
+    Item(std::string name, int quantity, int price)
+            : name(name), quantity(quantity), price(price) {
+        static int IdCounter=0;
+        id = IdCounter++;
+    }
+
+    int getId() const { return id; }
+    std::string getName() const { return name; }
+    int getQuantity() const { return quantity; }
+    int getPrice() const { return price; }
+
+private:
+    int id;
+    std::string name;
+    int quantity;
+    int price;
+};
