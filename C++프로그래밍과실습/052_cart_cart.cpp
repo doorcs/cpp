29f74aff0d5c89cc04e634a24eccc3cf47299217
성문 <castledoor@pusan.ac.kr>
2024-01-20 02:42:05 +0900

    과제 052: 객체를 이용한 Cart

---
+++ C++프로그래밍과실습/052_cart_cart.cpp
@@ -0,0 +1,28 @@
+#include "052_cart_cart.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void Cart::addItem(const Item &item) {
+    items.push_back(item);
+}
+void Cart::deleteItem(int id) {
+    items.erase(remove_if(items.begin(), items.end(), [id](auto a){return a.getId() == id;}), items.end());
+}
+int Cart::checkout() {
+    Checkout tmp;
+    return tmp.calculateTotalPrice(items.begin(), items.end());
+}
+void Cart::viewItemDetails(int id, std::ostream &out) const {
+    auto it = find_if(items.begin(), items.end(), [id](auto a){return a.getId() == id;});
+    if(it != items.end()){
+        out << "Item " << id << ": " <<endl;
+        out << "Name: " << it->getName() << endl;
+        out << "Quantity: " << it->getQuantity() << endl;
+        out << "Price: " << it->getPrice() << endl;
+    }
+    else out << "Item not found." << endl;
+}
+
+// const_iterator Cart::cbegin() const {}
+// const_iterator Cart::cend() const {}
