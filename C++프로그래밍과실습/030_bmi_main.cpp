716984111b9fc15c6b421363902b69b374984cc2
성문 <castledoor@pusan.ac.kr>
2024-01-21 00:20:38 +0900

    과제 030: BMI

---
+++ C++프로그래밍과실습/030_bmi_main.cpp
@@ -0,0 +1,42 @@
+// main.cpp 제공
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+#include <algorithm>
+#include "030_bmi.h"
+using namespace std;
+
+int main() {
+    int rep;
+    cin >> rep;
+    vector<unique_ptr<PatientInfo>> patients;
+
+    for(int i=0; i<rep; i++){
+        PatientInfo newPatient;
+        cin >> newPatient.name >> newPatient.age >> newPatient.weight >> newPatient.height;
+
+        addinfo(newPatient, patients);
+    }
+
+    printAll(patients);
+    cout << endl;
+
+    cout << "Enter the name: ";
+    string query;
+    cin >> query;
+    searchInfo(patients, query);
+    cout << endl;
+
+    cout << "Enter the BMI threshold: ";
+    double threshold;
+    cin >> threshold;
+    printAbove(patients, threshold);
+    cout << endl;
+
+    cout << "Enter the BMI threshold: ";
+    cin >> threshold;
+    remove(patients, threshold);
+
+    printAll(patients);
+}
