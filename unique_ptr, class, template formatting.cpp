9f1f742d288cbdc5d4096c8e62bbed01f4a2e234
성문 <castledoor@pusan.ac.kr>
2024-01-23 01:35:42 +0900

    배운 내용들 응용해보기
    두들낙서 Youtube:
    https://youtu.be/DYSEulQoj8Q?si=K69bwFbQAXOjMvqw
    
    김태연 교수님 Mooc:
    https://www.edwith.org/pnu-modernC/lecture/1346523

---
+++ unique_ptr, class, template formatting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip> // setw(), setprecision()
+#include <memory>
+using namespace std;
+
+template <typename T>
+class Test{
+    T* data;
+public:
+    Test(): data(new T){
+        cout << setw(43) << "Constructor" << endl; // setw()는 포메팅해서 출력하려는 문자열 왼쪽에 써줘야한다!
+    }
+    ~Test() noexcept{
+        delete data;
+        cout << setw(43) << "Destructor" << endl;
+    }
+};
+
+int main(){
+    cout << "=============================== main() begins =================================" << endl;
+    cout << "======================== create unique_ptr<Test> `a` ==========================" << endl;
+    unique_ptr<Test<int>> a(new Test<int>());
+    cout << "==== unique_ptr<Test> `b` takeover the ownership from unique_ptr<Test> `a` ====" << endl;
+    unique_ptr<Test<int>> b(a.release());
+    cout << "========= unique_ptr<Test> `b` points new data after releasing old one ========" << endl;
+    b.reset(new Test<int>());
+    cout << "================================ main() ends ==================================" << endl;
+}
