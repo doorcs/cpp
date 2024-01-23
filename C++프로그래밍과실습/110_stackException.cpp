e981b2d7103b326ad7536ac9b88d6b83e4bd8542
성문 <castledoor@pusan.ac.kr>
2024-01-23 23:04:58 +0900

    과제 110: Exception - Stack
    테케들 묶어서 주석처리

---
+++ C++프로그래밍과실습/110_stackException.cpp
@@ -0,0 +1,37 @@
+#include "110_stackException.h"
+
+////// 테스트케이스 1 출력: main.cpp:[라인번호]:Exception: Stack is empty
+// int main() {
+//    Stack<char> stack1(1);
+//    stack1.push('a');
+//    stack1.pop();
+//    stack1.pop();
+//    stack1.print();
+// }
+
+////// 테스트케이스 2 출력: main.cpp:[라인번호]:Exception: Stack is full!
+// int main() {
+//     Stack<char> stack2(1);
+//     stack2.push('a');
+//     stack2.push('b');
+//     stack2.print();
+// }
+
+////// 테스트케이스 3 출력: 2
+// int main() {
+//     Stack<int> stack3(1);
+//     stack3.push(1);
+//     stack3.pop();
+//     stack3.push(2);
+//     stack3.print();
+//     stack3.pop();
+// }
+
+////// 테스트케이스 4 출력: 3
+// int main() {
+//     Stack<int> stack4(2);
+//     stack4.push(1); stack4.push(2);
+//     stack4.pop();    stack4.pop();
+//     stack4.push(3);
+//     stack4.print();
+// }
