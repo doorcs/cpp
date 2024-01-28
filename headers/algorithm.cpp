c01743866d5010ed1ebf20f291db8813fcbd8be1
성문 <castledoor@pusan.ac.kr>
2024-01-28 20:26:36 +0900

    <algorithm>
    계속 추가해나가기

---
+++ headers/algorithm.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::sort();
+    std::reverse();
+    std::remove();
+    std::unique();
+    std::find();
+    std::copy();
+    std::move();
+    std::swap();
+
+
+    std::min();
+    std::max();
+    std::minmax();
+
+    std::min_element();
+    std::max_element();
+    std::minmax_element();
+    
+
+    std::fill();
+    
+    // ...
+  }
