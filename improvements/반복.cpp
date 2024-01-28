6aab238b739ad1605845cbb91cd7c9da9b98d42a
성문 <castledoor@pusan.ac.kr>
2024-01-28 19:39:31 +0900

    무언가를 특정 횟수만큼 반복할 때: for문 | while문
    개인적으로는 for문 방식이 편한 듯

---
+++ improvements/반복.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rep;
+    cin >> rep;
+
+    // 방법 1
+    for(int i=0; i<rep; i++){
+      ; // do something
+    }
+
+    // 방법 2
+    while(rep--){
+      ; // do something
+    }
+}
